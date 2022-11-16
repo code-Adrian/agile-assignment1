3712eb39baa75a9cc97c631690674f2bf12441ba
Adrian Bernacki <adrianbernacki2021@gmail.com>
2022-11-16 19:02:06 +0000

    e2e - login test implemented and tested

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,58 @@
+const authUser = require('../fixtures/login.json');
+import "../support/commands";
+
+describe("Testing Logging in", () => {
+    const {email,password} = authUser;
+
+    describe("Logging in", () => {
+    it("check if the url is /login", () => {
+        cy.visit("/");
+        cy.checkUrl("/login");
+    });
+    it("Credentials input", () => {
+        cy.get("input[name='email']").clear().type(email);
+        cy.get("input[name='password']").clear().type(password);
+        cy.get("button[type='submit']").click();
+     });
+    it("Check if logged in & sign out", () => {
+        cy.checkPageHeader("Discover Movies");
+        cy.get(".MuiCardHeader-content").should("have.length", 20);
+        cy.get("button").contains("Sign Out!").click();
+    });
+    it("Check if redirected back to /login route.", () => {
+        cy.checkUrl("/login");
+    });
+    });
+
+    describe("Error checking", () => {
+        beforeEach(() => {
+            cy.get("input[name='email']").clear();
+            cy.get("input[name='password']").clear();
+          });
+
+       it("Already exists check", () => {
+           cy.visit("/");
+           cy.checkUrl("/login")
+           cy.get("button").contains("Signup").click();
+           cy.get("h1").contains("Register")
+           cy.get("input[name='email']").type(email);
+           cy.get("input[name='password']").type(password);
+           cy.get("button[type='submit']").click();
+           cy.get("h1").contains("auth/email-already-in-use");
+       });
+
+       it("Missing Email check", () => {
+           cy.checkUrl("/signup");
+           cy.get("input[name='password']").type(password);
+           cy.get("button[type='submit']").click();
+           cy.get("h1").contains("auth/email-already-in-use");
+       });
+
+       it("Missing Password check", () => {
+           cy.checkUrl("/signup");
+           cy.get("input[name='email']").type(email);
+           cy.get("button[type='submit']").click();
+           cy.get("h1").contains("auth/internal-error");
+       });
+   });
+});
--- cypress/e2e/sampletest.cy.js
+++
@@ -1,5 +0,0 @@
-describe("Sample test for GitLab", () => {
-    it("Console log test", () => {
-        cy.log("Test");
-    });
-});
